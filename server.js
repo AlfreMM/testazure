8300353089422170e201850cc2bc077f0db3bafe
Alfredo Moreno <alfredomm@outlook.com>
2022-10-29 21:02:44 +0200

    .

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello world!');
 });
-server.listen(5001);
+server.listen(process.env.PORT);
